cb00bce3e4bb91c5a2b646917e8c07b61a783932
Pedro <35297069+pedrosilva410@users.noreply.github.com>
2022-05-31 09:26:20 +0200

    about page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -6,11 +6,29 @@ const About = () => {
     <main>
       <h1>About</h1>
       <p className="about__p">
-        You are free to use this template for whatever you want, from creating
-        your own website or make a cool app with it. I didn't changed a lot from
-        the real create react ap, I simply deleted most of the useless stuff and
-        added react-router-dom to make a simple navigation (the one you used to
-        get here)
+      Japanese domestic market (JDM) refers to Japan's home market for vehicles.
+      <br/>
+      <br/>
+      Japanese domestic market vehicles may differ greatly from the cars that Japanese
+      manufacturers build for export and vehicles derived from the same platforms built
+      in other countries. The Japanese car owner looks more toward innovation than
+      long-term ownership which forces Japanese carmakers to refine new technologies
+      and designs first in domestic vehicles. For instance, the 2003 Honda Inspire
+      featured the first application of Honda's Variable Cylinder Management. However,
+      the 2003 Honda Accord V6, which was the same basic vehicle, primarily intended
+      for the North American market, did not feature VCM, which had a poor reputation
+      after Cadillac's attempt in the 1980s with the V8-6-4 engine. VCM was
+      successfully introduced to the Accord V6 in its redesign for 2008.
+      <br/>
+      <br/>
+      In 1988, JDM cars were limited by voluntary self-restraints among manufacturers
+      to 280 horsepower (PS) (276 hp) and a top speed of 180 km/h (111.8 mph),
+      limits imposed by the Japan Automobile Manufacturers Association (JAMA) for safety.
+      The horsepower limit was lifted in 2004 but the speed limit of 180 km/h (111.8 mph) remains.
+      <br/>
+      <br/>
+      This are just some facts that may or may not be in the quiz I putted together, I hope
+      you will have fun playing it and learn something about JDM :)
       </p>
     </main>
   );
