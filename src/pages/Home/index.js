b5439085e49ede4fffed6ceaf27f0b0f276c7540
Pedro <35297069+pedrosilva410@users.noreply.github.com>
2022-05-31 09:04:28 +0200

    Barebones

---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+const Home = () => {
+  return (
+    <main>
+      <h1>React Template</h1>
+      <p className="home__p">
+        This is a base template created with react create app
+      </p>
+    </main>
+  );
+};
+
+export default Home;
